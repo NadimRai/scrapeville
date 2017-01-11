052ed369315c48204d21da8485590a53de0cb5ea
NadimRai <rai_nadim@yahoo.com>
2017-01-10 19:59:22 -0800

    Added Devise User model and Username field to Devise form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :newsvilles
   root to: 'pages#index'
    #root to: 'application#google_news'
