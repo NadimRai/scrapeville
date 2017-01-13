e0175ad7749975ed19fe136c7fca23099e19d76c
NadimRai <rai_nadim@yahoo.com>
2017-01-12 17:46:49 -0800

    Added default image display ability in comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
    has_many :newsvilles
    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url:
-"/images/:style/missing.png" 
+"nia.png" 
 	validates_attachment_content_type :image, content_type:/\Aimage\/.*\Z/ 
 end
