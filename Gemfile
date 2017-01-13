d68af24c700492a00fd3790ab90b550a1ddd8a7e
NadimRai <rai_nadim@yahoo.com>
2017-01-12 16:22:14 -0800

    Added Figaro gem

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'sqlite3'
+  gem 'figaro', '~> 1.1', '>= 1.1.1'
 end
 
 group :production do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
- #   def google_news
-	#   	require 'open-uri'
-	#   	page = Nokogiri::HTML(open('https://news.google.com/news/'))
-	#  	render text: page
-	# end
+ rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path
+    flash[:alert] = "Sorry, you are not authorized to access this area!"
+  end
 end
